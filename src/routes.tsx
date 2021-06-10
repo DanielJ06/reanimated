f69453e57d08f1979ab99264cb88d6ba2d826e7f
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-06-10 16:32:06 -0300

    Setup stack navigation

---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator()
+
+const MainRoutes: React.FC = () => {
+  return (
+    <Stack.Navigator>
+      
+    </Stack.Navigator>
+  );
+}
+
+export default MainRoutes;
